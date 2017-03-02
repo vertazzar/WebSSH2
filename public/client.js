b976181dd565a6db2cc0282649b56ffdf74aac23
Edis <vertazzar@gmail.com>
2017-03-02 11:33:21 +0100

    once

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 var terminalContainer = document.getElementById('terminal-container'),
     term = new Terminal({
-	cursorBlink: true
+        cursorBlink: true
     }),
     socket,
     termid;
@@ -45,46 +45,48 @@ socket.on('connect', function() {
             socket.emit('data', data);
         });
 
+        socket.on('title', function(data) {
+            document.title = data;
+        }).on('status', function(data) {
+            document.getElementById('status').innerText = data;
+        }).on('headerBackground', function(data) {
+            document.getElementById('header').style.backgroundColor = data;
+        }).on('header', function(data) {
+            document.getElementById('header').innerText = data;
+        }).on('footer', function(data) {
+            document.getElementById('footer').innerText = data;
+        }).on('statusBackground', function(data) {
+            document.getElementById('status').style.backgroundColor = data;
+        }).on('allowreplay', function(data) {
+            console.log('allowreplay: ' + data);
+            if (data == 'true') {
+                document.getElementById('credentials').style.display = 'inline';
+                console.log('display: block');
+            } else {
+                document.getElementById('credentials').style.display = 'none';
+                console.log('display: none');
+            }
+        }).on('data', function(data) {
+            term.write(data);
+        }).on('disconnect', function() {
+            document.getElementById('status').style.backgroundColor = 'red';
+            document.getElementById('status').innerText = 'WEBSOCKET SERVER DISCONNECTED (will retry)';
+            //socket.io.reconnection(false);
+        }).on('error', function(err) {
+            document.getElementById('status').style.backgroundColor = 'red';
+            document.getElementById('status').innerText = 'ERROR ' + err + ' will retry after 5 seconds';
+            errors += 1;
+            if (errors > 20) {
+                document.getElementById('status').innerText = 'ERROR ' + err + ' - failed retrying after 20 times.';
+                return;
+            }
+            setTimeout(function() {
+                authorize();
+            }, 4000);
+        });
+
         listens = true;
     }
     authorize();
-    socket.on('title', function(data) {
-        document.title = data;
-    }).on('status', function(data) {
-        document.getElementById('status').innerText = data;
-    }).on('headerBackground', function(data) {
-        document.getElementById('header').style.backgroundColor = data;
-    }).on('header', function(data) {
-        document.getElementById('header').innerText = data;
-    }).on('footer', function(data) {
-        document.getElementById('footer').innerText = data;
-    }).on('statusBackground', function(data) {
-        document.getElementById('status').style.backgroundColor = data;
-    }).on('allowreplay', function(data) {
-        console.log ('allowreplay: ' + data);
-        if (data == 'true') {
-            document.getElementById('credentials').style.display = 'inline';
-            console.log ('display: block');
-        } else {
-            document.getElementById('credentials').style.display = 'none';
-	    console.log ('display: none');
-        }
-    }).on('data', function(data) {
-        term.write(data);
-    }).on('disconnect', function() {
-        document.getElementById('status').style.backgroundColor = 'red';
-        document.getElementById('status').innerText = 'WEBSOCKET SERVER DISCONNECTED (will retry)';
-        //socket.io.reconnection(false);
-    }).on('error', function(err) {
-        document.getElementById('status').style.backgroundColor = 'red';
-        document.getElementById('status').innerText = 'ERROR ' + err + ' will retry after 5 seconds';
-        errors += 1;
-        if (errors > 20) {
-            document.getElementById('status').innerText = 'ERROR ' + err + ' - failed retrying after 20 times.';
-            return;
-        }
-        setTimeout(function () {
-            authorize();
-        }, 4000);
-    });
+
 });
